1f54b44b9b5341c200d2d52a96c6536010226b79
basma21trid-cpu <basma21trid@gmail.com>
2025-10-23 15:40:25 +0100

    realisation

--- REALISATION 4/REALISATION.js
+++ REALISATION 4/REALISATION.js
@@ -45,70 +45,11 @@ btn.addEventListener("click", () => {
 });
 
  afficherTaches();
-/*let input = document.getElementById("nouvelleTache");
-let btn = document.getElementById("btn-ajouter");
-let liste = document.getElementById("Liste");
 
 
-btn.addEventListener("click", () => {
-    let texte = input.value.trim();
-    if(texte === "") return;
 
-    let tache = document.createElement("li");
 
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    
-    let span = document.createElement("span");
-    span.innerText = texte;
 
-    let bouton = document.createElement("button");
-    bouton.innerText = "DELETE";
-
-    bouton.addEventListener("click",() => {
-    tache.remove();
-    });
-
-    tache.appendChild(checkbox);
-    tache.appendChild(span);
-    tache.appendChild(bouton);
-
-    liste.appendChild(tache);
-    
-    input.value = "";
-
-})*/
-
-/*let input = document.getElementById("nouvelleTache");
-let btn = document.getElementById("btn-ajouter");
-let liste = document.getElementById("liste");
-
-btn.addEventListener("click", () => {
-    let texte = input.value;
-    if(texte === "") return;
-
-    let tache = document.createElement("li");
-
-    let cb = document.createElement("input");
-    cb.type = "checkbox";
-    tache.appendChild(cb);
-
-    let span = document.createElement("span");
-    span.innerText = texte;
-    tache.appendChild(span);
-
-    let bouton = document.createElement("button");
-    bouton.innerText = "DELETE";
-    tache.appendChild(bouton);
-
-    
-    bouton.addEventListener("click", () => {
-        liste.removeChild(tache);
-    });
-
-    liste.appendChild(tache);
-    input.value = "";
-});*/
 
 
 
